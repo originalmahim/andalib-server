ea7693b8c1882d8fdafabfa1dab4130960bd1daf
originalmahim <tareqazizmahim@gmail.com>
2024-02-21 00:02:13 +0600

    Conected mongodb with server side successfully

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mahim:9ec3Peya9zwRGxvV@cluster0.gvou2sg.mongodb.net/?retryWrites=true&w=majority";
 
-  
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
 
 
 
